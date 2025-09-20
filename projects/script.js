6a8def9fe23453ec8ffde629a3571827427c1dca
B2GPT <132028674+Moderatorboy@users.noreply.github.com>
2025-09-21 02:09:37 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio B2GPT";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
@@ -136,4 +136,5 @@ document.onkeydown = function (e) {
     }
 
 }
+
 
